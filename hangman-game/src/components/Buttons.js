e70bf6386f00b0027c07e85c702a7de583a49b85
czhexg <czhexg1@gmail.com>
2022-05-22 14:21:30 +0800

    updated buttons

--- hangman-game/src/components/Buttons.js
+++ hangman-game/src/components/Buttons.js
@@ -7,8 +7,6 @@ function Buttons({
     mistake,
     updateMistake,
 }) {
-    const letters = "QWERTYUIOPASDFGHJKLZXCVBNM".split("");
-
     const handleGuess = (e) => {
         if (!answer.split("").includes(e.target.value)) {
             updateMistake(mistake + 1);
@@ -18,24 +16,79 @@ function Buttons({
         }
     };
 
-    return letters.map((letter) => (
-        <button
-            type="button"
-            value={letter}
-            disabled={right.has(letter) || wrong.has(letter)}
-            onClick={handleGuess}
-            className={
-                wrong.has(letter)
-                    ? "btn btn-danger"
-                    : right.has(letter)
-                    ? "btn btn-success"
-                    : "btn btn-outline-dark"
-            }
-            style={{ height: "50px", width: "50px" }}
-        >
-            {letter}
-        </button>
-    ));
+    return (
+        <div className="container">
+            <div className="row mt-3">
+                <div className="col text-center">
+                    {"QWERTYUIOP".split("").map((letter) => (
+                        <button
+                            type="button"
+                            value={letter}
+                            disabled={right.has(letter) || wrong.has(letter)}
+                            onClick={handleGuess}
+                            className={
+                                "ms-2 btn btn-lg " +
+                                (wrong.has(letter)
+                                    ? "btn-danger"
+                                    : right.has(letter)
+                                    ? "btn-success"
+                                    : "btn-outline-dark")
+                            }
+                            style={{ height: "50px", width: "50px" }}
+                        >
+                            {letter}
+                        </button>
+                    ))}
+                </div>
+            </div>
+            <div className="row mt-3">
+                <div className="col text-center">
+                    {"ASDFGHJKL".split("").map((letter) => (
+                        <button
+                            type="button"
+                            value={letter}
+                            disabled={right.has(letter) || wrong.has(letter)}
+                            onClick={handleGuess}
+                            className={
+                                "ms-2 btn btn-lg " +
+                                (wrong.has(letter)
+                                    ? "btn-danger"
+                                    : right.has(letter)
+                                    ? "btn-success"
+                                    : "btn-outline-dark")
+                            }
+                            style={{ height: "50px", width: "50px" }}
+                        >
+                            {letter}
+                        </button>
+                    ))}
+                </div>
+            </div>
+            <div className="row mt-3">
+                <div className="col text-center">
+                    {"ZXCVBNM".split("").map((letter) => (
+                        <button
+                            type="button"
+                            value={letter}
+                            disabled={right.has(letter) || wrong.has(letter)}
+                            onClick={handleGuess}
+                            className={
+                                "ms-2 btn btn-lg " +
+                                (wrong.has(letter)
+                                    ? "btn-danger"
+                                    : right.has(letter)
+                                    ? "btn-success"
+                                    : "btn-outline-dark")
+                            }
+                            style={{ height: "50px", width: "50px" }}
+                        >
+                            {letter}
+                        </button>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Buttons;
